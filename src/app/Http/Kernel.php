f4fafc8f2880b01c34069fe7a7bbbe29df0fa40d
KostaDokosic <kosta99dt@gmail.com>
2022-05-13 12:18:02 +0200

    cors

--- src/app/Http/Kernel.php
+++ src/app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\Cors;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel {
@@ -13,6 +14,7 @@ class Kernel extends HttpKernel {
      * @var array
      */
     protected $middleware = [
+        \Fruitcake\Cors\HandleCors::class,
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
