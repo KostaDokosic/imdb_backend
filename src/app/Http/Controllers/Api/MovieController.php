836189abe82df5ec000188384f35fd6feecbb025
KostaDokosic <kosta99dt@gmail.com>
2022-05-16 14:34:25 +0200

    added movie collection

--- src/app/Http/Controllers/Api/MovieController.php
+++ src/app/Http/Controllers/Api/MovieController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\MovieCollection;
 use App\Models\Movie\Movie;
 use Illuminate\Http\Request;
 
@@ -15,7 +16,7 @@ class MovieController extends Controller
      */
     public function index()
     {
-        return response(Movie::all(), 200);
+        return response(new MovieCollection(Movie::all()), 200);
     }
 
     /**
