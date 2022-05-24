39a5fc7db922c31c18cb6c15568f907894ad44d0
KostaDokosic <kosta99dt@gmail.com>
2022-05-24 14:01:14 +0200

    fixed likes/dislikes + added ability to remove like/dislike

--- src/app/Http/Controllers/Api/LikeController.php
+++ src/app/Http/Controllers/Api/LikeController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LikeRequest;
 use App\Http\Resources\LikeResource;
 use App\Models\Like\Like;
+use App\Models\Movie\Movie;
 use Illuminate\Http\Request;
 
 class LikeController extends Controller
@@ -25,10 +26,22 @@ class LikeController extends Controller
         $data = $request->validated();
 
         $oldLikes = Like::all()->where('movie_id', $data['movie_id'])->where('user_id', $request->user()->id);
-        if($oldLikes->count() > 0) {
+        if($oldLikes->count() > 0)
+        {
             $oldLike = $oldLikes->first();
-            $oldLike->like = $data['like'];
-            return LikeResource::collection($oldLikes);
+            if($data['like'] === -1) {
+                $oldLike->delete();
+                return [
+                    'like' => -1,
+                    'totalLikes' => Movie::find($data['movie_id'])->likes->where('like', 1)->count(),
+                    'totalDislikes' => Movie::find($data['movie_id'])->likes->where('like', 0)->count()
+                ];
+            }
+            else {
+                $oldLike->like = $data['like'];
+                $oldLike->save();
+                return new LikeResource($oldLike);
+            }
         }
 
         $newLike = Like::create([
@@ -36,7 +49,7 @@ class LikeController extends Controller
             'user_id' => $request->user()->id,
             'like' => $data['like']
         ]);
-        return LikeResource::make($newLike);
+        return new LikeResource($newLike);
     }
 
     /**
